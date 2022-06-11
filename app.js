d089ac945e87537d551314dc7edcdf4efbcdc384
eyezek <71928231+eyezek@users.noreply.github.com>
2022-06-11 16:47:51 -0400

    still clueless, still learning

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const gameBoard = (function () {
   "use strict";
   const board = ["", "", "", "", "", "", "", "", ""];
 
-  const setSquare = function (array, marker) {
-    array[index] = player.marker;
+  const setSquare = function (index, marker) {
+    board[index] = marker;
   };
   return {
     board,
@@ -26,15 +26,16 @@ const playerFactory = function (name, marker) {
   return { name, marker };
 };
 
-playerFactory("player1", "X");
+const player1 = playerFactory("player1", "X");
 
-playerFactory("player2", "O");
+const player2 = playerFactory("player2", "O");
 
 const displayController = (function () {
+  "use strict";
   // placeholder
 })();
 
-const gameFactory = (function () {
+const gameModule = (function () {
   "use strict";
 
   const endTurn = function () {
@@ -56,6 +57,24 @@ const gameFactory = (function () {
       return;
     } else if (square9.innerHTML == "X") {
       return;
+    } else if (square1.innerHTML == "O") {
+      return;
+    } else if (square2.innerHTML == "O") {
+      return;
+    } else if (square3.innerHTML == "O") {
+      return;
+    } else if (square4.innerHTML == "O") {
+      return;
+    } else if (square5.innerHTML == "O") {
+      return;
+    } else if (square6.innerHTML == "O") {
+      return;
+    } else if (square7.innerHTML == "O") {
+      return;
+    } else if (square8.innerhtml == "O") {
+      return;
+    } else if (square9.innerHTML == "O") {
+      return;
     } else {
     }
   };
@@ -63,69 +82,96 @@ const gameFactory = (function () {
   const player1Turn = function () {
     square1.addEventListener("click", function () {
       square1.innerHTML = "X";
+      gameBoard.setSquare(1, player1.marker);
+      endTurn();
     });
-    endTurn();
     square2.addEventListener("click", function () {
       square2.innerHTML = "X";
+      gameBoard.setSquare(2, player1.marker);
+      endTurn();
     });
-    endTurn();
     square3.addEventListener("click", function () {
       square3.innerHTML = "X";
+      gameBoard.setSquare(3, player1.marker);
+      endTurn();
     });
-    endTurn();
     square4.addEventListener("click", function () {
       square4.innerHTML = "X";
+      gameBoard.setSquare(4, player1.marker);
+      endTurn();
     });
-    endTurn();
     square5.addEventListener("click", function () {
       square5.innerHTML = "X";
+      gameBoard.setSquare(5, player1.marker);
+      endTurn();
     });
-    endTurn();
     square6.addEventListener("click", function () {
       square6.innerHTML = "X";
+      gameBoard.setSquare(6, player1.marker);
+      endTurn();
     });
-    endTurn();
     square7.addEventListener("click", function () {
       square7.innerHTML = "X";
+      gameBoard.setSquare(7, player1.marker);
+      endTurn();
     });
-    endTurn();
     square8.addEventListener("click", function () {
       square8.innerHTML = "X";
+      gameBoard.setSquare(8, player1.marker);
+      endTurn();
     });
-    endTurn();
     square9.addEventListener("click", function () {
       square9.innerHTML = "X";
+      gameBoard.setSquare(9, player1.marker);
+      endTurn();
     });
-    endTurn();
   };
 
   const player2Turn = function () {
     square1.addEventListener("click", function () {
       square1.innerHTML = "O";
+      gameBoard.setSquare(1, player2.marker);
+      endTurn();
     });
     square2.addEventListener("click", function () {
       square2.innerHTML = "O";
+      gameBoard.setSquare(2, player2.marker);
+      endTurn();
     });
     square3.addEventListener("click", function () {
       square3.innerHTML = "O";
+      gameBoard.setSquare(3, player2.marker);
+      endTurn();
     });
     square4.addEventListener("click", function () {
       square4.innerHTML = "O";
+      gameBoard.setSquare(4, player2.marker);
+      endTurn();
     });
     square5.addEventListener("click", function () {
       square5.innerHTML = "O";
+      gameBoard.setSquare(5, player2.marker);
+      endTurn();
     });
     square6.addEventListener("click", function () {
       square6.innerHTML = "O";
+      gameBoard.setSquare(6, player2.marker);
+      endTurn();
     });
     square7.addEventListener("click", function () {
       square7.innerHTML = "O";
+      gameBoard.setSquare(7, player2.marker);
+      endTurn();
     });
     square8.addEventListener("click", function () {
       square8.innerHTML = "O";
+      gameBoard.setSquare(8, player2.marker);
+      endTurn();
     });
     square9.addEventListener("click", function () {
       square9.innerHTML = "O";
+      gameBoard.setSquare(9, player2.marker);
+      endTurn();
     });
   };
   return {
@@ -135,124 +181,12 @@ const gameFactory = (function () {
   };
 })();
 
-// let gameBoard = {
-//   board: [
-//     square1,
-//     square2,
-//     square3,
-//     square4,
-//     square5,
-//     square6,
-//     square7,
-//     square8,
-//     square9,
-//   ],
-// };
+function game() {
+  gameBoard.board;
+  gameBoard.setSquare();
+  gameModule.endTurn();
+  gameModule.player1Turn();
+  gameModule.player2Turn();
+}
 
-// function endTurn() {
-//   if (square1.innerhtml == "X") {
-//     return;
-//   } else if (square2.innerHTML == "X") {
-//     return;
-//   } else if (square3.innerHTML == "X") {
-//     return;
-//   } else if (square4.innerHTML == "X") {
-//     return;
-//   } else if (square5.innerHTML == "X") {
-//     return;
-//   } else if (square6.innerHTML == "X") {
-//     return;
-//   } else if (square7.innerHTML == "X") {
-//     return;
-//   } else if (square8.innerHTML == "X") {
-//     return;
-//   } else if (square9.innerHTML == "X") {
-//     return;
-//   } else {
-//   }
-// }
-
-// function player1Turn() {
-//   square1.addEventListener("click", function () {
-//     square1.innerHTML = "X";
-//   });
-//   endTurn();
-//   square2.addEventListener("click", function () {
-//     square2.innerHTML = "X";
-//   });
-//   endTurn();
-//   square3.addEventListener("click", function () {
-//     square3.innerHTML = "X";
-//   });
-//   endTurn();
-//   square4.addEventListener("click", function () {
-//     square4.innerHTML = "X";
-//   });
-//   endTurn();
-//   square5.addEventListener("click", function () {
-//     square5.innerHTML = "X";
-//   });
-//   endTurn();
-//   square6.addEventListener("click", function () {
-//     square6.innerHTML = "X";
-//   });
-//   endTurn();
-//   square7.addEventListener("click", function () {
-//     square7.innerHTML = "X";
-//   });
-//   endTurn();
-//   square8.addEventListener("click", function () {
-//     square8.innerHTML = "X";
-//   });
-//   endTurn();
-//   square9.addEventListener("click", function () {
-//     square9.innerHTML = "X";
-//   });
-//   endTurn();
-// }
-
-// function player2Turn() {
-//   square1.addEventListener("click", function () {
-//     square1.innerHTML = "O";
-//   });
-//   square2.addEventListener("click", function () {
-//     square2.innerHTML = "O";
-//   });
-//   square3.addEventListener("click", function () {
-//     square3.innerHTML = "O";
-//   });
-//   square4.addEventListener("click", function () {
-//     square4.innerHTML = "O";
-//   });
-//   square5.addEventListener("click", function () {
-//     square5.innerHTML = "O";
-//   });
-//   square6.addEventListener("click", function () {
-//     square6.innerHTML = "O";
-//   });
-//   square7.addEventListener("click", function () {
-//     square7.innerHTML = "O";
-//   });
-//   square8.addEventListener("click", function () {
-//     square8.innerHTML = "O";
-//   });
-//   square9.addEventListener("click", function () {
-//     square9.innerHTML = "O";
-//   });
-// }
-
-// player1Turn();
-
-// function game() {}
-
-// let gameBoard = {
-//     square1: document.getElementById("square1"),
-//     square2: document.getElementById("square2"),
-//     square3: document.getElementById("square3"),
-//     square4: document.getElementById("square4"),
-//     square5: document.getElementById("square5"),
-//     square6: document.getElementById("square6"),
-//     square7: document.getElementById("square7"),
-//     square8: document.getElementById("square8"),
-//     square9: document.getElementById("square9"),
-//   };
+game();
