7d366a58e1765eea77803e957201a71b38121730
eyezek <71928231+eyezek@users.noreply.github.com>
2022-06-15 16:14:34 -0400

    still progressing slowly but surely...

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const gameBoard = (function () {
     board[index] = marker;
   };
 
-  const checkForWin = function (board) {
+  const checkForWin = function () {
     const winningConditions = [
       [board[0], board[1], board[2]],
       [board[3], board[4], board[5]],
@@ -41,15 +41,17 @@ const gameBoard = (function () {
       return element === "O";
     }
 
-    for (let i = 0; i <= winningConditions.length; i++) {
+    for (let i = 0; i < winningConditions.length; i++) {
       if (winningConditions[i].every(isX) || winningConditions[i].every(isO)) {
         winner = true;
+        console.log(winner);
+        break;
       } else {
         winner = false;
+        console.log(winner);
       }
-
-      return winner;
     }
+    return winner;
   };
 
   return {
@@ -112,7 +114,7 @@ const gameModule = (function () {
         event.target.getAttribute("data-index-number"),
         currentPlayer.marker
       );
-      gameBoard.checkForWin(gameBoard.board);
+      gameBoard.checkForWin();
       displayController.displayWinner();
       switchPlayers();
       displayController.displayPlayer();
@@ -129,3 +131,125 @@ function game() {
 }
 
 game();
+
+// let gameBoard = {
+//   board: [
+//     square1,
+//     square2,
+//     square3,
+//     square4,
+//     square5,
+//     square6,
+//     square7,
+//     square8,
+//     square9,
+//   ],
+// };
+
+// function endTurn() {
+//   if (square1.innerhtml == "X") {
+//     return;
+//   } else if (square2.innerHTML == "X") {
+//     return;
+//   } else if (square3.innerHTML == "X") {
+//     return;
+//   } else if (square4.innerHTML == "X") {
+//     return;
+//   } else if (square5.innerHTML == "X") {
+//     return;
+//   } else if (square6.innerHTML == "X") {
+//     return;
+//   } else if (square7.innerHTML == "X") {
+//     return;
+//   } else if (square8.innerHTML == "X") {
+//     return;
+//   } else if (square9.innerHTML == "X") {
+//     return;
+//   } else {
+//   }
+// }
+
+// function player1Turn() {
+//   square1.addEventListener("click", function () {
+//     square1.innerHTML = "X";
+//   });
+//   endTurn();
+//   square2.addEventListener("click", function () {
+//     square2.innerHTML = "X";
+//   });
+//   endTurn();
+//   square3.addEventListener("click", function () {
+//     square3.innerHTML = "X";
+//   });
+//   endTurn();
+//   square4.addEventListener("click", function () {
+//     square4.innerHTML = "X";
+//   });
+//   endTurn();
+//   square5.addEventListener("click", function () {
+//     square5.innerHTML = "X";
+//   });
+//   endTurn();
+//   square6.addEventListener("click", function () {
+//     square6.innerHTML = "X";
+//   });
+//   endTurn();
+//   square7.addEventListener("click", function () {
+//     square7.innerHTML = "X";
+//   });
+//   endTurn();
+//   square8.addEventListener("click", function () {
+//     square8.innerHTML = "X";
+//   });
+//   endTurn();
+//   square9.addEventListener("click", function () {
+//     square9.innerHTML = "X";
+//   });
+//   endTurn();
+// }
+
+// function player2Turn() {
+//   square1.addEventListener("click", function () {
+//     square1.innerHTML = "O";
+//   });
+//   square2.addEventListener("click", function () {
+//     square2.innerHTML = "O";
+//   });
+//   square3.addEventListener("click", function () {
+//     square3.innerHTML = "O";
+//   });
+//   square4.addEventListener("click", function () {
+//     square4.innerHTML = "O";
+//   });
+//   square5.addEventListener("click", function () {
+//     square5.innerHTML = "O";
+//   });
+//   square6.addEventListener("click", function () {
+//     square6.innerHTML = "O";
+//   });
+//   square7.addEventListener("click", function () {
+//     square7.innerHTML = "O";
+//   });
+//   square8.addEventListener("click", function () {
+//     square8.innerHTML = "O";
+//   });
+//   square9.addEventListener("click", function () {
+//     square9.innerHTML = "O";
+//   });
+// }
+
+// player1Turn();
+
+// function game() {}
+
+// let gameBoard = {
+//     square1: document.getElementById("square1"),
+//     square2: document.getElementById("square2"),
+//     square3: document.getElementById("square3"),
+//     square4: document.getElementById("square4"),
+//     square5: document.getElementById("square5"),
+//     square6: document.getElementById("square6"),
+//     square7: document.getElementById("square7"),
+//     square8: document.getElementById("square8"),
+//     square9: document.getElementById("square9"),
+//   };
